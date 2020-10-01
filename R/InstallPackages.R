e234fafebd24fcab7d2c12b0c0591d117561138e
Jean-Baptiste Lecomte <lecomte.jeanbaptiste@gmail.com>
2020-10-01 09:50:25 +0200

    add arugments to force install of packages

--- R/InstallPackages.R
+++ R/InstallPackages.R
@@ -1,11 +1,17 @@
 #' Install package if not present
 #'
 #' @param Packages
-#'
+#' @param force
 #' @return
 #' @export
 #'
 #' @examples
-InstallPackages <- function(Packages) {
+InstallPackages <- function(Packages, force = FALSE) {
+  if(force == FALSE){
   sapply(Packages, function(Package) if (!Package %in% installed.packages()[, 1]) {install.packages(Package)})
+  }
+
+  if(force == TRUE){
+    sapply(Packages, function(Package) {install.packages(Package)})
+  }
 }
