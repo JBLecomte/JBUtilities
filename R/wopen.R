58b396b4a0612da5e2e2dbd5da5c062853de64b3
Jean-Baptiste Lecomte <lecomte.jeanbaptiste@gmail.com>
2020-10-02 11:20:36 +0200

    fix call to get_os function

--- R/wopen.R
+++ R/wopen.R
@@ -1,21 +1,20 @@
 #' Open a windows explorer of the relative path
-#' Save plot with specific needs i.e. plot for a Beamer presentation
 #'
-#' @param relative_path a charachter string for the directory where to save the plot
+#' @param relative_path a character string for the directory where to save the plot
 #' @import here
 #' @return open the relative path
 #' @export
-#'
+
 wopen <- function(relative_path){
 
   options(warn=-1)
   relative_path <-  paste0( here::here(), '/', relative_path)
 
-  if(get.os() == 'windows'){
+  if(get_os() == 'windows'){
     y <- gsub("/", "\\\\", relative_path)
     shell(paste0("explorer ", y), intern = FALSE)
   }
-  if(get.os() == 'linux'){
+  if(get_os() == 'linux'){
     system(paste0("xdg-open ", relative_path),
            intern = TRUE,
            ignore.stdout = TRUE,
