9c81dc64224ee6e01128a6ada05f14b5d8e45075
Jean-Baptiste Lecomte <lecomte.jeanbaptiste@gmail.com>
2020-10-29 16:44:14 +0100

    add repo for installing packages

--- R/InstallPackages.R
+++ R/InstallPackages.R
@@ -2,16 +2,17 @@
 #'
 #' @param Packages
 #' @param force
+#' @param repository
 #' @return
 #' @export
 #'
 #' @examples
-InstallPackages <- function(Packages, force = FALSE) {
+InstallPackages <- function(Packages, force = FALSE, repository = "http://cran.cnr.berkeley.edu/") {
   if(force == FALSE){
-  sapply(Packages, function(Package) if (!Package %in% installed.packages()[, 1]) {install.packages(Package)})
+  sapply(Packages, function(Package) if (!Package %in% installed.packages()[, 1]) {install.packages(Package, repos = repository)})
   }
 
   if(force == TRUE){
-    sapply(Packages, function(Package) {install.packages(Package)})
+    sapply(Packages, function(Package) {install.packages(Package,  repos = repository)})
   }
 }
