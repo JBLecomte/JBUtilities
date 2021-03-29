711622b7a08911bec4bb674791255a712fc6bb87
JBLecomte <lecomte.jeanbaptiste@gmail.com>
2021-03-29 11:53:55 +0200

    update theme with title and caption aligned

--- R/simple_ggplot2_theme.R
+++ R/simple_ggplot2_theme.R
@@ -6,15 +6,21 @@ simple_ggplot2_theme <- function (base_size = 11,
                                   base_family = '') {
 
   ### Build theme on the ggplot2 theme black and white : theme_bw
-  simple.ggplot2.theme <- ggplot2::theme_bw(base_family=base_family, base_size=base_size)
+  simple.ggplot2.theme <- ggplot2::theme_bw(base_family = base_family,
+                                            base_size = base_size)
 
 
   ### Change black and white theme
   simple.ggplot2.theme <- simple.ggplot2.theme +
+
     theme(
 
       ### Remove the square line around the plot
       panel.border = element_rect( fill = NA, colour = NA),
+
+      ### Margin
+      #### margin of 25 but at bottom 10
+      plot.margin = margin(t = 25, r = 25, b = 10, l = 25, unit = "pt"),
 
       ### Grid on the plot
       panel.grid = element_blank(),
@@ -40,14 +46,23 @@ simple_ggplot2_theme <- function (base_size = 11,
       ### Legend
       legend.background = element_rect( colour = NA, fill = NA),
       legend.key = element_rect( colour = NA, fill = NA),
-      legend.key.size = unit( 1.1, 'line'),
-      legend.title = element_text( size = rel(1), face = 'bold'),
+      legend.key.size = unit( 1.1, "line"),
+      legend.title = element_text(size = rel(1), face = "bold"),
       legend.text = element_text(size = rel(1)),
+      legend.position = "bottom",
 
       ### Facet
       strip.text.x = element_text(size = 6),
       strip.text.y = element_text(size = 6),
-      strip.background = element_rect(colour = "black", fill = NA)
+      strip.background = element_rect(colour = "black", fill = NA),
+
+      ### Title
+      #### left aligned title
+      plot.title.position = "plot",
+
+      ### Caption
+      #### right aligned caption
+      plot.caption.position = "plot"
     )
 }
 
